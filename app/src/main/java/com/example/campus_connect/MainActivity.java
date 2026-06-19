0c8bbc3b88d19642d9557219ce2b585c1b3b7095
Vraj Desai <desaivraj18@gmail.com>
2026-06-19 23:59:34 +0530

    splash screen added

--- app/src/main/java/com/example/campus_connect/MainActivity.java
+++ app/src/main/java/com/example/campus_connect/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.campus_connect;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Looper;
+import android.os.Handler;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        int splashDuraton = 3000;
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, login_screen.class);
+                startActivity(intent);
+
+                finish();
+            }
+        },splashDuraton);
     }
 }
