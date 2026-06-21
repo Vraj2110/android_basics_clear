c37330cd60478de34ddd836bcd80d7a5ca59fbd5
Vraj Desai <desaivraj18@gmail.com>
2026-06-21 13:39:36 +0530

    signup page attach and next wull be forget password page

--- app/src/main/java/com/example/campus_connect/login_screen_1.java
+++ app/src/main/java/com/example/campus_connect/login_screen_1.java
@@ -1,6 +1,9 @@
 package com.example.campus_connect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,16 @@ public class login_screen_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_screen1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView signuptxt = findViewById(R.id.txtSignupLink);
+
+        signuptxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_screen_1.this, signup.class);
+
+                startActivity(intent);
+            }
         });
     }
 }
